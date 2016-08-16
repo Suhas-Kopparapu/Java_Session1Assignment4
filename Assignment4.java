227cf3fa0bb0238efcf9c71ce93709297fd66bb3
Suhas-Kopparapu <kopparapu.suhas@gmail.com>
2016-08-17 01:42:22 +0530

    Session1Assignment4

---
+++ Assignment4.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+public class Assignment4 {
+public static void main(String[] args){
+	//values of the expressions
+	int x=5,y=10;
+	int a,b,c,d;
+	a= x+(y*2);
+	b=x-y+2;
+	c=(x+y)*2;
+	d=(y%x);
+	//printing the value of equation a
+	System.out.println(a);
+	//printing the value of expression b
+	System.out.println(b);
+	//printing the value of expression c
+	System.out.println(c);
+	//printing the remainder of expression d
+	System.out.println(d);
+}
+}
